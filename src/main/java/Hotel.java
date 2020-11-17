5768708e40ae866dc4828112d47b58dfb245d62b
PrajwalRao29 <raoprajrao@gmail.com>
2020-11-17 23:00:44 +0530

    Added functionality to create a hotel class

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,10 @@
+public class Hotel {
+    public String hotelName;
+    public double rate;
+
+    public Hotel(String hotelName, int rate) {
+        this.hotelName = hotelName;
+        this.rate = rate;
+    }
+}
+
